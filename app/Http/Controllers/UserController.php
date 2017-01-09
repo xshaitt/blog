6fed75f3d93fa9d13229f00a7ec6f4b5ab595a64
imac <773999579@qq.com>
2017-01-09 11:24:48 +0800

    添加用户登录功能

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Validator;
 
 class UserController extends Controller
 {
@@ -18,13 +20,48 @@ class UserController extends Controller
             'alpha_num'=>':attribute只能为字母或者数字',
             'confirmed'=>'两次输入的密码不一致',
             'between'=>':attribute最小:min个字符,最大:max个字符',
+            'name.unique'=>'用户名已被注册',
         ];
         $this->validate($request,[
-            'name'=>'required|alpha_num|between:4,8',
+            'name'=>'required|alpha_num|between:4,8|unique:users,name',
             'password'=>'required|alpha_num|confirmed|between:4,8',
             'password_confirmation'=>'required',
         ],$message);
         $result = User::create($request->all());
-        dd($result);
+        if($result)
+        {
+            return redirect('/');
+        }else{
+            return redirect('reg');
+        }
+    }
+    public function login(){
+        return view('login');
+    }
+    public function doLogin(Request $request)
+    {
+        $name = $request->get('name');
+        $password = $request->get('password');
+        if(Auth::attempt(['name'=>$name,'password'=>$password]))
+        {
+            return redirect()->intended('/');
+        }
+        $request->session()->flash('loginError','用户名或者密码错误');
+        return redirect('login');
+    }
+    public function profile()
+    {
+        return view('profile',['user'=>Auth::user()->toArray()]);
+    }
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+    public function loginId()
+    {
+        Auth::logout();
+        Auth::loginUsingId(1);
+        return redirect('profile');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ Route::get('/', function () {
 
 Route::get('reg','UserController@reg');
 Route::post('createUser','UserController@createUser');
+Route::get('login','UserController@login');
+Route::post('login','UserController@doLogin');
+Route::get('profile','UserController@profile')->middleware('auth');
+Route::get('logout','UserController@logout');
+Route::get('loginid','UserController@loginId');
